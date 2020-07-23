a1e7b2bca6bfed48897e340f03b4e8dafb88bee1
Mitch Henderson <shiftymitch@gmail.com>
2020-07-22 19:49:58 -0600

    Issue7: Added React Public & Src Starter Files

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="container">
+            {/* Insert Home Page Elements / Components */}
+        </div>
+    )
+}
+
+export default Home;
