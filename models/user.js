ae1faf3c22194b98c2b1a64e2308b15377cf1782
nabeek <nabeek@gmail.com>
2020-07-29 22:04:25 -0600

    Added new event and listed existing events

--- models/user.js
+++ models/user.js
@@ -2,21 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email_is_verified: {
-        type: Boolean,
-        default: false
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    state: String,
-    county: String
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email_is_verified: {
+    type: Boolean,
+    default: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  state: String,
+  county: String,
+  events: [{ type: Schema.Types.ObjectId, ref: "Events" }],
 });
 
 const User = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Bcrypt = require("bcryptjs");
 const axios = require("axios");
 
 const app = express();
+const routes = require("./routes");
 const db = require("./models");
 
 const PORT = process.env.PORT || 3001;
@@ -12,56 +13,57 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(routes);
+
 // ! Routes for testing
 app.post("/api/user", (req, res) => {
-    db.User.create(req.body)
-        .then(dbUser => {
-            res.sendStatus(200);
-            console.log("DBUSER:", dbUser)
-        })
-        .catch(err => console.log(err))
-})
-
-app.post("/api/event", (req, res) => {
-    db.Event.create(req.body)
-        .then(dbUser => {
-            res.sendStatus(200);
-            console.log("DBUSER:", dbUser)
-        })
-        .catch(err => console.log(err))
-})
+  db.User.create(req.body)
+    .then(dbUser => {
+      res.sendStatus(200);
+      console.log("DBUSER:", dbUser);
+    })
+    .catch(err => console.log(err));
+});
 
 //! Coronavirus API testing
 app.get("/api/current/:state", (req, res) => {
-    axios.get("https://covidtracking.com/api/v1/states/"
-    + req.params.state
-    + "/current.json")
-      .then(response => {
-        res.json(response.data);
-      })
-      .catch(e => console.log(e));
-})
+  axios
+    .get(
+      "https://covidtracking.com/api/v1/states/" +
+        req.params.state +
+        "/current.json"
+    )
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(e => console.log(e));
+});
 
 //! Connect to Database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/contact_tracing_db", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/contact_tracing_db",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
-})
+    useCreateIndex: true,
+  }
+);
 
 //! serve static assets
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./build'));
-    // server index.html if `/about` reached -> assets served through `express.static`
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname, './build/index.html')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./build"));
+  // server index.html if `/about` reached -> assets served through `express.static`
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "./build/index.html"))
+  );
 } else {
-    //! Home Route
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, './client/public/index.html'))
-    })
+  //! Home Route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`)
+  console.log(`Server listening on PORT: ${PORT}`);
 });
