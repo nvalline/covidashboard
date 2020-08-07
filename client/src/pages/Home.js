54928311515e0d3de3b7ec2fd953b98bca246905
Mitch Henderson <shiftymitch@gmail.com>
2020-08-07 09:28:00 -0600

    Issue77: Updating styling on Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -47,68 +47,72 @@ function Home() {
   
 
   return (
-    <div className="container-fluid">
-      <p className="text-center">{userEmail} / {userState} / {userCounty} County</p>
-      <div className="events m-3">
-          <div className="text-center">
+    <div className="">
+      <div className="row user-info">
+        <div className="col icon">
+          <span><i className="fa fa-user-circle-o"></i> {userEmail}
+          </span>
+        </div>
+        <div className="col icon">
+        <span><i className="fa fa-map-marker"></i> {userState} / {userCounty} County
+        </span>
+        </div>
+      </div>
+      <div className="sections">
+        <div className="row">
+          {/* Trend */}
+          <div id="trend" className="col section">
+            <h4 className="section-title">Current Trend</h4>
+          </div>
+          {/* Cases */}
+          <div id="cases" className="col section">
+            <h4 className="section-title">Positive Cases</h4>
             <div className="row">
-            </div>
-            <div className="row">
-              {/* Trend */}
-              <div id="trend" className="col section">
-                <h4 className="section-title">Trend</h4>
+              <div className="col">
+                <p>Total</p>
+                <p className="data-result">{stateData.positive === undefined ? "N/A" : stateData.positive.toLocaleString()}</p>
               </div>
-              {/* Cases */}
-              <div id="cases" className="col section">
-                <h4 className="section-title">Cases</h4>
-                <div className="row">
-                  <div className="col">
-                    <p>Total</p>
-                    <p className="data-result">{stateData.positive === undefined ? "N/A" : stateData.positive.toLocaleString()}</p>
-                  </div>
-                  <div className="col">
-                    <p>New</p>
-                    <p className="data-result">{stateData.positiveIncrease}</p>
-                  </div>
-                </div>
-                <p>{userCounty} County: (Get County Data)</p>
+              <div className="col">
+                <p>New</p>
+                <p className="data-result">{stateData.positiveIncrease}</p>
               </div>
             </div>
+            <p>{userCounty} County: (Get County Data)</p>
+          </div>
+        </div>
 
-            <div className="row">
-              {/* Symptoms */}
-              <div id="symptoms" className="col section">
-                <h4 className="section-title">Symptoms</h4>
-                <Symptoms />
-              </div>
-              {/* Events */}
-              <div id="events" className="col section">
-                <h4 className="section-title">Events</h4>
-                  { 
-                  events.length === 0 
-                  ? (
-                    <div className="text-center mb-5">
-                      <p>No events added yet.</p>
-                    </div>
-                  ) 
-                  : (
-                    events.map(event => (
-                    <div className="row dash-event">
-                      <Link to="/events"><p className="dash-event-title" style={{color: "black"}}>{event.title}</p></Link>
-                      <p className="dash-event-date">{moment(event.date).calendar()}</p>
-                    </div>
-                    ))
-                  )
-                }
-                <Link to="/new" className="btn btn-lg btn-primary">
-                  + Add A New Event
-                </Link>
-              </div>
+        <div className="row">
+          {/* Symptoms */}
+          <div id="symptoms" className="col section">
+            <h4 className="section-title">Symptoms</h4>
+            <div className="text-left">
+              <Symptoms />
             </div>
           </div>
-        {/* {tempEvents.map(event => (
-                    <Event key={event.id} value={event.value} title={event.title} date={event.date} />
-                ))} */}
+          {/* Events */}
+          <div id="events" className="col section">
+            <h4 className="section-title">Watched Events</h4>
+              { 
+              events.length === 0 
+              ? (
+                <div className="text-center mb-5">
+                  <p>No events added yet.</p>
+                </div>
+              ) 
+              : (
+                events.map(event => (
+                <div className="dash-event">
+                  <Link to="/events"><p className="dash-event-title" style={{color: "black"}}>{event.title}</p></Link>
+                  <p className="dash-event-date">{moment(event.date).calendar()}</p>
+                </div>
+                ))
+              )
+            }
+            <Link to="/new" className="btn btn-primary mb-3">
+              + Add A New Event
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
