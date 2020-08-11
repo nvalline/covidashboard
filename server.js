91a4ea54cde652965a250ec5ea1f04c88892d7dd
Mitch Henderson <shiftymitch@gmail.com>
2020-08-10 20:12:01 -0600

    fixing heroku build

--- server.js
+++ server.js
@@ -53,13 +53,9 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("./client/build"));
     app.get("*", (req, res) =>
-        res.sendFile(path.join(__dirname, "./build/index.html"))
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
     );
-} else {
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./client/public/index.html"));
-    });
-}
+} 
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: ${PORT}`);
