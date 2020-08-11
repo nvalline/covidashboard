8e4d94150b50b172df0f0a5cd069d3abca4c4b41
Mitch Henderson <shiftymitch@gmail.com>
2020-08-10 21:06:40 -0600

    fixing heroku build

--- server.js
+++ server.js
@@ -52,10 +52,10 @@ app.use("/auth", auth);
 const routes = require("./routes");
 app.use(routes);
 
-app.use(express.static("client/build"));
-app.use("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "./client/build/index.html"))
-);
+// app.use(express.static("client/build"));
+// app.use("*", (req, res) =>
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"))
+// );
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
