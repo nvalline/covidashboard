203521ef85dcd0af3b325ef10affd052745e6ccc
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-04-01 16:32:17 -0600

    downgraded connect-mongo to v3

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-const express = require("express");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const mongoose = require("mongoose");
-const path = require("path");
-const axios = require("axios");
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const path = require('path');
+const axios = require('axios');
 
-const api = require("./routes/api");
-const auth = require("./routes/auth");
-const passport = require("./config/passport");
+const api = require('./routes/api');
+const auth = require('./routes/auth');
+const passport = require('./config/passport');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 // Connect to Database
-const MONGO_LOCAL_URI = require("./config/keys").MongoURI;
+const MONGO_LOCAL_URI = require('./config/keys').MongoURI;
 
 mongoose
-    .connect(process.env.MONGODB_URI || MONGO_LOCAL_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-    .then(console.log(`MongoDB connected`))
-    .catch(err => console.log(err));
+	.connect(process.env.MONGODB_URI || MONGO_LOCAL_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	})
+	.then(console.log(`MongoDB connected`))
+	.catch((err) => console.log(err));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Express Session
 app.use(
-    session({
-        secret: "three blind mice",
-        resave: true,
-        saveUninitialized: true,
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
-    })
+	session({
+		secret: 'three blind mice',
+		resave: true,
+		saveUninitialized: true,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
+	})
 );
 
 // Passport Middleware
@@ -44,19 +44,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use("/api", api);
-app.use("/auth", auth);
+app.use('/api', api);
+app.use('/auth', auth);
 
-const routes = require("./routes");
+const routes = require('./routes');
 app.use(routes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) =>
-        res.sendFile(path.join(__dirname, "./index.html"))
-    );
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(path.join(__dirname, './index.html'))
+	);
 }
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
+	console.log(`Server listening on PORT: ${PORT}`);
 });
